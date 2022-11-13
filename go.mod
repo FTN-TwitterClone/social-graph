e4c896a2d478e5923dca12e102381e38b910a662
borisavz <borisavzivanovic@gmail.com>
2022-11-13 12:54:36 +0100

    Added mTLS, CORS and refactored Neo4j connection logic

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module social-graph
 
-go 1.19
+go 1.18
 
 require (
 	github.com/gorilla/handlers v1.5.1
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	gorillaHandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"log"
 	"net/http"
 	"os"
@@ -12,62 +11,50 @@ import (
 	"social-graph/controller"
 	"social-graph/repository/neo4jRepo"
 	"social-graph/service"
+	"social-graph/tls"
 	"time"
 )
 
-type Neo4jConfiguration struct {
-	Url      string
-	Username string
-	Password string
-	Database string
-}
-
-func (nc *Neo4jConfiguration) newDriver() neo4j.Driver {
-	result, err := neo4j.NewDriver(nc.Url, neo4j.BasicAuth(nc.Username, nc.Password, ""))
-	if err != nil {
-		panic(err)
-	}
-	return result
-}
-
 func main() {
-
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "8080"
+	repositoryNeo4j, err := neo4jRepo.NewRepositoryNeo4j()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	configuration := parseConfiguration()
-	repositoryNeo4j := neo4jRepo.RepositoryNeo4j{Driver: configuration.newDriver(), Database: configuration.Database}
-
-	socialGraphService := service.NewSocialGraphService(&repositoryNeo4j)
+	socialGraphService := service.NewSocialGraphService(repositoryNeo4j)
 
 	socialGraphController := controller.NewSocialGraphController(socialGraphService)
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/social-graph/follows", socialGraphController.CreateFollow).Methods("POST")
-	router.HandleFunc("/api/social-graph/follows", socialGraphController.RemoveFollow).Methods("DELETE")
-	router.HandleFunc("/api/social-graph/following/{username}", socialGraphController.GetFollowing).Methods("GET")
-	router.HandleFunc("/api/social-graph/followers/{username}", socialGraphController.GetFollowers).Methods("GET")
+	router.HandleFunc("/follows", socialGraphController.CreateFollow).Methods("POST")
+	router.HandleFunc("/follows", socialGraphController.RemoveFollow).Methods("DELETE")
+	router.HandleFunc("/following/{username}", socialGraphController.GetFollowing).Methods("GET")
+	router.HandleFunc("/followers/{username}", socialGraphController.GetFollowers).Methods("GET")
 
-	cors := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}))
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 
-	//Initialize the server
-	server := http.Server{
-		Addr:         ":" + port,
-		Handler:      cors(router),
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+	// start server
+	srv := &http.Server{
+		Addr:      "0.0.0.0:8000",
+		Handler:   handlers.CORS(allowedHeaders, allowedMethods, allowedOrigins)(router),
+		TLSConfig: tls.GetHTTPServerTLSConfig(),
 	}
 
-	log.Println("Server listening on port", port)
 	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Fatal(err)
+		log.Println("server starting")
+
+		certFile := os.Getenv("CERT")
+		keyFile := os.Getenv("KEY")
+
+		if err := srv.ListenAndServeTLS(certFile, keyFile); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
 		}
 	}()
+
 	sigCh := make(chan os.Signal)
 	signal.Notify(sigCh, os.Interrupt)
 	signal.Notify(sigCh, os.Kill)
@@ -77,26 +64,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	if server.Shutdown(ctx) != nil {
+	if srv.Shutdown(ctx) != nil {
 		log.Fatal("Cannot gracefully shutdown...")
 	}
 	log.Println("Server stopped")
-}
-
-func parseConfiguration() *Neo4jConfiguration {
-	return &Neo4jConfiguration{
-		Url:      lookupEnvOrGetDefault("NEO4J_URI", "neo4j://localhost:7687"),
-		Username: lookupEnvOrGetDefault("NEO4J_USER", "neo4j"),
-		Password: lookupEnvOrGetDefault("NEO4J_PASSWORD", "matematika1000"),
-		Database: "neo4j",
-	}
-
-}
-
-func lookupEnvOrGetDefault(key string, defaultValue string) string {
-	if env, found := os.LookupEnv(key); !found {
-		return defaultValue
-	} else {
-		return env
-	}
 }
